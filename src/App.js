e7e11f44db17e63c82d8358485ca7a17b37e29cb
Michel Emad Wakim <micho.courses@gmail.com>
2020-12-01 20:15:13 +0200

    the passing data

---
+++ src/App.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+import './App.css';
+import logo from './logo.svg';
+
+
+const profiles = [
+  {
+    id: 1,
+    userID: '1',
+    favoriteMovieID: '1',
+  },
+  {
+    id: 2,
+    userID: '2',
+    favoriteMovieID: '1',
+  },
+  {
+    id: 3,
+    userID: '4',
+    favoriteMovieID: '5',
+  },
+  {
+    id: 4,
+    userID: '5',
+    favoriteMovieID: '2',
+  },
+  {
+    id: 5,
+    userID: '3',
+    favoriteMovieID: '5',
+  },
+  {
+    id: 6,
+    userID: '6',
+    favoriteMovieID: '4',
+  },
+];
+
+const users = {
+  1: {
+    id: 1,
+    name: 'Jane Cruz',
+    userName: 'coder',
+  },
+  2: {
+    id: 2,
+    name: 'Matthew Johnson',
+    userName: 'mpage',
+  },
+  3: {
+    id: 3,
+    name: 'Autumn Green',
+    userName: 'user123',
+  },
+  4: {
+    id: 4,
+    name: 'John Doe',
+    userName: 'user123',
+  },
+  5: {
+    id: 5,
+    name: 'Lauren Carlson',
+    userName: 'user123',
+  },
+  6: {
+    id: 6,
+    name: 'Nicholas Lain',
+    userName: 'user123',
+  },
+};
+
+const movies = {
+  1: {
+    id: 1,
+    name: 'Planet Earth 1',
+  },
+  2: {
+    id: 2,
+    name: 'Selma',
+  },
+  3: {
+    id: 3,
+    name: 'Million Dollar Baby',
+  },
+  4: {
+    id: 4,
+    name: 'Forrest Gump',
+  },
+  5: {
+    id: 5,
+    name: 'Get Out',
+  },
+};
+
+const CardList = ({users, movies, profiles}) => {
+	return(
+      <div>
+      	<ul>
+          {profiles.map(profile => {
+            const userName = users[profile.userID].name;
+            const favouriteMovie = movies[profile.favoriteMovieID].name;
+            return (
+              <li key={profile.id}>
+                <p>{`${userName}'s favorite movie is "${favouriteMovie}."`}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    )
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2 className="App-title">This app is to pass data from many arrays to form a list of them. </h2>
+        </header>
+        <h2>Favorite Movies</h2>
+			<CardList profiles={profiles} movies={movies} users={users}/>
+      </div>
+    );
+  }
+}
+
+export default App;
+
